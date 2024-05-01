0324530c264b6108bbc09071644a841b27bd4292
anangyoga <aharwinto@gmail.com>
2024-05-01 10:40:30 +0700

    Route & View

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Page</title>
+</head>
+<body>
+    <h1>Homework</h1>
+    <p>the guy told me to create this page so I did.</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// we can return string like this
+Route::get('/string', function () {
+    return "This paragraph is actually just a string that's rendered on the page";
+});
+
+// we can also return json
+Route::get('/json', function(){
+    return ['foo'=> 'bar'];
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
