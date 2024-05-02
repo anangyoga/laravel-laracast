234eae59f96918346ca08181e649d0e9b543d855
anangyoga <aharwinto@gmail.com>
2024-05-02 22:29:28 +0700

    Layouting with Blade Component

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,4 +1,4 @@
 {{-- x prefix to ensure that they'r unique, not interfere with existing html tag --}}
 <x-layout>
-    <h1>This is the contact page</h1>
+    <h1>This is the about page</h1>
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/contact', function(){
+    return view('contact');
+});
+
+Route::get('/about', function(){
+    return view('about');
 });
 
 // we can return string like this
@@ -16,6 +24,3 @@ Route::get('/json', function(){
     return ['foo'=> 'bar'];
 });
 
-Route::get('/contact', function(){
-    return view('contact');
-});
