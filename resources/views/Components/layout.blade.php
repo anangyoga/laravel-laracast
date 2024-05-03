cfe0475da9616eb1aa39e2b445f493f395b1c49c
anangyoga <aharwinto@gmail.com>
2024-05-03 23:05:02 +0700

    Homework: dynamic link component with  and

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -7,9 +7,10 @@
 </head>
 <body>
     <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
-        <a href="/contact">Contact</a>
+       <x-nav-link href="/">Home</x-nav-link>
+       <x-nav-link href="/about" style="color: greenyellow">About</x-nav-link>
+       <x-nav-link href="/contact">Contact</x-nav-link>
+       
     </nav>
    
     {{-- @php
