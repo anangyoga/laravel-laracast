0ed60743f88b88c5d10b4a4f1420cb1e14c6298d
anangyoga <aharwinto@gmail.com>
2024-05-05 11:16:59 +0700

    Homework: conditional rendering a and button tags

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -17,13 +17,13 @@
             </div>
             <div class="hidden md:block">
               <div class="ml-10 flex items-baseline space-x-4">
-                <x-nav-link href='/' :active="request()->is('/')">
+                <x-nav-link href='/' :active="request()->is('/')" >
                   Home
                 </x-nav-link>
                 <x-nav-link href='/about' :active="request()->is('about')" >
                   About
                 </x-nav-link>
-                <x-nav-link href='/contact' :active="request()->is('contact')">
+                <x-nav-link :active="request()->is('contact')" type="button">
                   Contact
                 </x-nav-link>
               </div>
