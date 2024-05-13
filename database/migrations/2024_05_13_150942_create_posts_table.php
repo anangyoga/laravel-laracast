5b522fc73f26bbc693e5090c6f5cfe13ddb6b0d2
anangyoga <aharwinto@gmail.com>
2024-05-13 22:14:52 +0700

    Eloquent: Model first, Migration second

---
+++ database/migrations/2024_05_13_150942_create_posts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
+
+// this Migration generated when making Model
+// then we can create the column
+// and don't forget to run 'php artisan migrate' so the data is pushed to TablePlus
