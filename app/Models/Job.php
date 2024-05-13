451573e262d1ac75f0e377fd9d0aecb408b08d75
anangyoga <aharwinto@gmail.com>
2024-05-13 22:07:46 +0700

    Eloquent: Tinker

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -12,4 +12,29 @@ class Job extends Model
 {
     // calling table 'job_listings' because the name isn't the same with Job model. this is how we handle if the name is different
     protected $table = 'job_listings';
+
+    // all attributes are allowed to be mass assign
+    protected $fillable = ['title', 'salary'];
+    // above scenario could happen when we insert new data to database using Tinker
 };
+
+// WE CAN DO THIS with Tinker. Tinker is like CLI for playing
+// > $job = App\Models\Job::find(2)
+// = App\Models\Job {#5116
+//     id: 2,
+//     title: "Scientist",
+//     salary: "$80,000",
+//     created_at: null,
+//     updated_at: null,
+//   }
+
+// > $job->title
+// = "Scientist"
+
+// > $job->id
+// = 2
+
+// > $job->delete()
+// = true
+
+// NOTE: create Model first, then create Migration
