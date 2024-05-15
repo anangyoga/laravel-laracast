6a834823dbbd17597824a9a304f6c5ee4384cfa5
anangyoga <aharwinto@gmail.com>
2024-05-15 21:20:37 +0700

    Model Factories: create JobFactory and extends to Job Models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -3,13 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 // Convention over Configuration
 // if the table name is comments, model should be Comment
 // if the table name is posts, model should be Post
 class Job extends Model
 {
+    // without this, there will be error: BadMethodCallException  Call to undefined method
+    use HasFactory;
+
     // calling table 'job_listings' because the name isn't the same with Job model. this is how we handle if the name is different
     protected $table = 'job_listings';
 
