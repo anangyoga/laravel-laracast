91454205a3d70bf71075484c4d9ea8890f29ebfb
anangyoga <aharwinto@gmail.com>
2024-05-15 21:20:00 +0700

    Model Factories: example UserFactory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -21,24 +21,40 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
+    // how to run: App\Models\User::factory(5)->create()
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'salary' => fake()->numerify(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            // if there's another data to run
+            'admin' => false,
         ];
     }
 
     /**
      * Indicate that the model's email address should be unverified.
      */
+
+    //how to run: App\Models\User::factory()->unverified()->create()
     public function unverified(): static
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    // if there's another method to run
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'admin' => true,
         ]);
     }
 }
