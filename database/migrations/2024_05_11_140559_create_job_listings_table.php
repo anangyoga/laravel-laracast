9fd0d3e5070bee829308c604a7c2fb8d552e32bc
anangyoga <aharwinto@gmail.com>
2024-05-16 11:20:27 +0700

    Relations: job listings belongsTo employers

--- database/migrations/2024_05_11_140559_create_job_listings_table.php
+++ database/migrations/2024_05_11_140559_create_job_listings_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Employer;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('job_listings', function (Blueprint $table) {
             $table->id();
+            // foreign id called employer_id
+            
+            // $table->unsignedBigInteger('employer_id');
+            // we use unisgnedBigInteger because whenever we call id method within migration, creating bigIntegerColumn
+            
+            // or we can use foreignIdFor that accepts Models
+            $table->foreignIdFor(App\Models\Employer::class);
+
             $table->string('title');
             $table->string('salary');
             $table->timestamps();
